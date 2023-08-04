6e09cca8426d4981158dbb32404a33f8598367c8
Almadny <Almadny2u@gmail.com>
2023-08-04 14:14:11 +0000

    Fixed Task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """ A Simple Flask Route """
 from flask import Flask, render_template
-from app import routes
 
 app = Flask(__name__)
 
 
-@app.route
-def index('/'):
+@app.route('/')
+def index():
     """ My index page route """
     return render_template('0-index.html')
 
