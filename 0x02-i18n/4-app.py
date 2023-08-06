be951efd7e6d0af03258bcde116cdb8a115b6876
Almadny <Almadny2u@gmail.com>
2023-08-06 14:21:05 +0000

    Cnfigured the page to use client language

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" A Simple Flask Route """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Defines page configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine the supported language """
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
+    if request.args.get('locale'):
+        if request.args['locale'] in app.config['LANGUAGES']:
+            return request.args['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGE'])
+
+
+@app.route('/')
+def index():
+    """ My index page route """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
