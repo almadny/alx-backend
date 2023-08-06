59150637f9930bd59d98d1f774c008f652de9c57
Almadny <Almadny2u@gmail.com>
2023-08-06 14:00:34 +0000

    Fixed pycodestyle

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,11 +3,13 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config(object):
     """ Defines page configuration """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
