084d645b4029caa4f251e62cb23e899a3566aded
Almadny <Almadny2u@gmail.com>
2023-08-06 13:58:33 +0000

    fixed some language settings

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ A Simple Flask Route """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 class Config(object):
